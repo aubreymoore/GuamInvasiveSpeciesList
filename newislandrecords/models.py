c54f8877f6f4bb83bb2da60515f2638ceac17932
aubreymoore <aubreymoore@guam.net>
2016-11-05 22:44:40 +1000

    safety

--- newislandrecords/models.py
+++ newislandrecords/models.py
@@ -1,5 +1,14 @@
 from django.db import models
+from django.utils.timezone import now
 
 class NewIslandRecord(models.Model):
     taxon = models.ForeignKey('taxonomy.Taxon')
     location = models.ForeignKey('locations.Location')
+    year_detected = models.IntegerField(null=True)
+    reference = models.ForeignKey('publications.Publication', null=True)
+    invasive_species = models.NullBooleanField()
+    established = models.NullBooleanField()
+    quarantine_interception = models.NullBooleanField()
+
+    def __str__(self):
+        return('{} {}'.format(self.taxon, self.year_detected))
